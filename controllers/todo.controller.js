24abdda2bb6b86989287dc4246d5befc7d2bf0e0
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-27 17:31:29 +0100

    skeleton built

---
+++ controllers/todo.controller.js
@@ -0,0 +1,49 @@
+// Get All To Dos
+exports.getList = async (req, res, next) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+// Get To Do By Id
+exports.getTodoById = async (req, res, next) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+// Post To Do
+exports.createTodo = async (req, res, next) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+// Put To Do By Id
+exports.updateTodo = async (req, res, next) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+// Delete By Id
+exports.deleteTodo = async (req, res, next) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: err.message || "Something went wrong. Please try again later.",
+    });
+  }
+};
