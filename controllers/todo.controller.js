a1eaecdf609a98f93c757b92c9fecf1f19bdf43b
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-27 19:06:51 +0100

    get all & get by id

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -4,6 +4,8 @@ const Todo = db.todos;
 // Get All To Dos
 exports.getList = async (req, res, next) => {
   try {
+    const todoList = await Todo.find();
+    res.status(200).json(todoList);
   } catch (err) {
     res.status(500).json({
       message: err.message || "Something went wrong. Please try again later.",
@@ -14,6 +16,14 @@ exports.getList = async (req, res, next) => {
 // Get To Do By Id
 exports.getTodoById = async (req, res, next) => {
   try {
+    let todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      res.status(404).json({
+        message: "To Do not found.",
+      });
+    }
+
+    res.status(200).json(todo);
   } catch (err) {
     res.status(500).json({
       message: err.message || "Something went wrong. Please try again later.",
