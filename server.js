74055529b799bd5d2410261b74849e39a04ba5e8
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-27 17:21:08 +0100

    server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// Import the Routes
+const todoRoutes = require("./routes/todo.route");
+
+const app = express();
+
+app.get("/api/todo", (res) => {
+  res.status(200).json({ message: "Welcome to the To Do List Rest API!" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not recognized." });
+});
+
+app.use("/api/todo/list", todoRoutes);
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "127.0.0.1";
+app.listen(PORT, () => console.log(`http://${HOST}:${PORT}`));
