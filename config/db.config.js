586443473e2ed224ba57ec1af1cfd1333f3d9e43
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-27 19:20:09 +0100

    config

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,10 @@
 const config = {
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  SECRET: process.env.SECRET,
+};
 
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-    SECRET: process.env.SECRET,
-  };
-  
-  config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.u9z6ifo.mongodb.net/${config.DB}?retryWrites=true&w=majority`;
-  
-  module.exports = config;
+config.URL = `mongodb+srv://${config.USER}:${config.PASSWORD}@cluster0.u9z6ifo.mongodb.net/${config.DB}?retryWrites=true&w=majority`;
+
+module.exports = config;
